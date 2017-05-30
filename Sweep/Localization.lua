aeda1a4de6a083e8d203b50fd6c02991c6d37c44
M2us <Dead_Maus@mail.ru>
2017-05-30 19:31:12 +0300

    Added support ruRU locale

--- Sweep/Localization.lua
+++ Sweep/Localization.lua
@@ -1,3 +1,17 @@
+if (GetLocale() == "ruRU") then
+	-- Russian localization by Lichery
+	
+	ADDON_LOADED = "Val's Sweep загружен. Введите |cffff0000/sweep|r для игры или |cffff0000/sweep help|r для инструкций."
+	MSG_0 = "Помощь для Val's Sweep"
+	MSG_1 = "- Откройте квадрат, щелкнув по нему. Если вы открываете мины, вы проигрываете."
+	MSG_2 = "- Число на квадрате указывает, сколько мин находится рядом с квадратом."
+	MSG_3 = "- Если вы считаете, что квадрат содержит мину, щелкните по нему правой кнопкой мыши."
+	MSG_4 = "- Когда все незаминированные клетки открыты, вы выигрываете."
+	MSG_5 = "- В игре "
+	MSG_6 = " мин."
+	GAME_OVER = "БОМБА! Игра окончена."
+	YOU_WIN = "Вы выиграли!"
+else
 	ADDON_LOADED = "Val's Sweep loaded. Type |cffff0000/sweep|r to play or |cffff0000/sweep help|r for instructions."
 	MSG_0 = "Help for Val's Sweep"
 	MSG_1 = "- Expose a square by clicking it. If you expose a mine, you lose."
@@ -8,3 +22,4 @@
 	MSG_6 = " mines per game."
 	GAME_OVER = "BOMB! Game over."
 	YOU_WIN = "You Win!"
+end
